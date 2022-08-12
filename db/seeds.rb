1550ecc599df1fb17fdb5131d4b2fee96f6066e0
John Rogers <john.rogers@mac.com>
2022-08-11 19:10:44 -0500

    ++ self cleaning seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flavor.destroy_all
+Product.destroy_all
+
+Flavor.create(name: "Snickers Swirl",
+              image_url: "https://cdn.culvers.com/menu-item-detail/img-Snicker-Swirl.png?w=120",
+              cf_id: "117")
+
+Location.create(latitude: "43.1245",
+                longitude: "-87.9129",
+                title: "Culver's of Glendale, WI - W Bayside Dr",
+                address: "300 West Bayside Drive",
+                city: "Glendale",
+                state: "WI",
+                postal_code: "53217",
+                cl_id: "852",
+                uid: "-372087291")
