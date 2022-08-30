14171c513230b82554ca474449835483001c26d3
John Rogers <john.rogers@mac.com>
2022-08-30 13:14:18 -0500

    cleaning up good job still, and validation to Flavor and Location

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,14 +1,5 @@
 class Flavor < ApplicationRecord
   has_many :location_flavors
   has_many :locations, through: :location_flavors
-
-  def load_data(data)
-    i = 0
-    data["response"]["collection"].each do
-      Flavor.create!(name: data["response"]["collection"][i]["flavor_day"],
-                     image_url: data["response"]["collection"][i]["flavor_image2"],
-                     cf_id: data["response"]["collection"][i]["flavor_id"])
-      i += 1
-    end
-  end
+  validates :cf_id, uniqueness: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,14 +1,5 @@
 class Location < ApplicationRecord
   has_many :location_flavors
   has_many :flavors, through: :location_flavors
-
-  # def image_url
-  #   # image_url = :flavors.image_url.where(location_id: id)
-  #   image_url = Flavors.image_url.where(location_id: id)
-  #   # image_url.where(location_id: id)
-  # end
-
-  # def name
-  #   name = :flavors[name].where(flavor_id: id)
-  # end
+  validates :cl_id, uniqueness: true
 end
