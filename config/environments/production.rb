8a4e7b1084561ed7912d09bae088e999e094bd4a
John Rogers <john.rogers@mac.com>
2023-08-26 17:06:24 -0500

    (っ▀¯▀)つ ++production.rb hosts configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Setting for url deployment location:
-  config.hosts = [
-    "api.flavorday.app"
-   ]
+  config.hosts << "api.flavorday.app"
   #config.action_controller.default_url_options = { host: 'api.flavorday.app' }
   
   # Code is not reloaded between requests.
