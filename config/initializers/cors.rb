d89c07c0f8e9bb379de4b8bda78a24f92072d773
John Rogers <john.rogers@mac.com>
2023-08-27 15:49:48 -0500

    (っ▀¯▀)つ testing settings in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #origins "localhost:8080", /https*:\/\/.*?flavorday\.app/
-    origins /https*:\/\/.*?flavorday\.app/
+    origins /\Ahttps*:\/\/.*?flavorday\.app\z/
 
     resource "*",
       headers: :any,
